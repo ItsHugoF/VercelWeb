835098ede09b7612b9554fa88abda20564b083c4
Hugo Fuentes Fernandez <hfuefer@aulaestudio.es>
2025-01-22 18:48:37 +0100

    Commit de la parte web

---
+++ main.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+
+  $("#botonMostrarAlumnos").click(function () {
+    $.ajax({
+      url: "/alumnos",
+      method: "GET",
+      success: function (alumnos) {
+        $("#listaAlumnos").empty();
+        alumnos.forEach(function (alumno) {
+          $("#listaAlumnos").append(
+            "<li>ID: " +
+              alumno.id +
+              ", Nombre: " +
+              alumno.nombre +
+              ", Apellido: " +
+              alumno.apellido +
+              ", Teléfono: " +
+              alumno.telefono +
+              "</li>"
+          );
+        });
+      },
+      error: function () {
+        alert("Error al obtener los alumnos");
+      },
+    });
+  });
+
+  $("#formularioAddAlumnos").submit(function (event) {
+    event.preventDefault();
+    const alumnoData = {
+      id: $("#alumnoId").val(),
+      nombre: $("#nombreAlumno").val(),
+      apellido: $("#apellidoAlumno").val(),
+      telefono: $("#telefonoAlumno").val(),
+    };
+    $.ajax({
+      url: "/agregarAlumno",
+      method: "POST",
+      contentType: "application/json",
+      data: JSON.stringify(alumnoData),
+      success: function (nuevoAlumno) {
+        $("#addMensajeAlumno").text("Alumno agregado correctamente");
+        $("#formularioAddAlumnos")[0].reset();
+      },
+      error: function (response) {
+        $("#addMensajeAlumno").text(
+          "Error al agregar alumno: " + response.responseJSON.message
+        );
+      },
+    });
+  });
+
+  $("#botonBuscarAlumno").click(function () {
+    const alumnoId = $("#idAlumnoBuscado").val();
+    if (!alumnoId) {
+      $("#resultado").text("Ingrese un ID de alumno");
+      return;
+    }
+    $.ajax({
+      url: "/alumnos/" + alumnoId,
+      method: "GET",
+      success: function (alumno) {
+        $("#resultado").html(
+          "<p>ID: " +
+            alumno.id +
+            "</p><p>Nombre: " +
+            alumno.nombre +
+            "</p><p>Apellido: " +
+            alumno.apellido +
+            "</p><p>Teléfono: " +
+            alumno.telefono +
+            "</p>"
+        );
+      },
+      error: function (response) {
+        $("#resultadoBuscado").text("Alumno no encontrado");
+      },
+    });
+  });
+});
